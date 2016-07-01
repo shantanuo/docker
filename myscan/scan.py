c3afe9c674b926f0e2e5a221b35a43c29039c4b3
Shantanu Oak <shantanu.oak@gmail.com>
2016-07-01 10:53:51 +0530

    Create scan.py

---
+++ myscan/scan.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import os.path
+import json
+import sys
+import string
+import pytesseract
+import re
+import difflib
+import csv
+import dateutil.parser as dparser
+from PIL import Image, ImageEnhance, ImageFilter
+
+path = sys.argv[1]
+
+img = Image.open(path)
+img = img.convert('RGBA')
+pix = img.load()
+
+for y in range(img.size[1]):
+    for x in range(img.size[0]):
+        if pix[x, y][0] < 102 or pix[x, y][1] < 102 or pix[x, y][2] < 102:
+            pix[x, y] = (0, 0, 0, 255)
+        else:
+            pix[x, y] = (255, 255, 255, 255)
+
+img.save('temp.jpg')
+
+text = pytesseract.image_to_string(Image.open('temp.jpg'))
+text = filter(lambda x: ord(x)<128,text)
+
+print text
