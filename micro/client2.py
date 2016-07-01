4622e5fe3de3bdc87d545be9b027b5ae2a244ee6
Shantanu Oak <shantanu.oak@gmail.com>
2016-07-01 11:14:21 +0530

    Create client2.py

---
+++ micro/client2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+TCP_IP = '23.21.167.60' # Get local machine name
+TCP_PORT = 8070                 # Reserve a port for your service.
+
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+with open('received_file', 'wb') as f:
+    print 'file opened'
+    while True:
+        #print('receiving data...')
+        data = s.recv(BUFFER_SIZE)
+        print('data=%s', (data))
+        if not data:
+            f.close()
+            print 'file close()'
+            break
+        # write data to a file
+        f.write(data)
+
+print('Successfully get the file')
+s.close()
+print('connection closed')
