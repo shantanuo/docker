c2473cd57db61995348ee7f8cc9c17315d5e018c
Shantanu Oak <shantanu.oak@gmail.com>
2016-07-01 11:51:21 +0530

    Update entrypoint.sh

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -1,1 +1,1 @@
-exec curl -s -XPOST localhost:9200/wikipedia_content/somelan/_bulk --data-binary @emlwiki-20160215-cirrussearch-content.json
+# exec curl -s -XPOST localhost:9200/wikipedia_content/somelan/_bulk --data-binary @emlwiki-20160215-cirrussearch-content.json
