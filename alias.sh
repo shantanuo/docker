3c31efb853ccf72f239ca57dba2516281ad46a23
Shantanu Oak <shantanu.oak@gmail.com>
2016-08-14 09:41:18 +0530

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,8 +1,9 @@
+echo "alias installmysql='docker run -d -p 3306:3306  -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  -v /my/custom:/etc/mysql/conf.d  -v /storage/test-mysql/datadir:/var/lib/mysql mysql:5.5'" >> ~/.bashrc
 
-alias installmysql='docker run -d -p 3306:3306  -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  -v /my/custom:/etc/mysql/conf.d  -v /storage/test-mysql/datadir:/var/lib/mysql mysql:5.5'
+echo "alias installelastic='docker run --name myelastic -v /tmp/:/usr/share/elasticsearch/config  -p 9200:9200 -p 9300:9300  -e ES_HEAP_SIZE=1g -d elasticsearch'" >> ~/.bashrc
 
-alias installelastic='docker run --name myelastic -v /tmp/:/usr/share/elasticsearch/config  -p 9200:9200 -p 9300:9300  -e ES_HEAP_SIZE=1g -d elasticsearch'
+echo "alias installkibana='docker run -d -p 5601:5601 --link myelastic:elasticsearch -e ELASTICSEARCH_URL=http://elasticsearch:9200 kibana'" >> ~/.bashrc
 
-alias installkibana='docker run -d -p 5601:5601 --link myelastic:elasticsearch -e ELASTICSEARCH_URL=http://elasticsearch:9200 kibana'
+echo "alias installlogstash='docker run -d -v "/tmp/logstash.conf":/usr/local/logstash/config/logstash.conf -v /tmp/:/tmp/ logstash -f /usr/local/logstash/config/logstash.conf'" >> ~/.bashrc
 
-alias installlogstash='docker run -d -v "/tmp/logstash.conf":/usr/local/logstash/config/logstash.conf -v /tmp/:/tmp/ logstash -f /usr/local/logstash/config/logstash.conf'
+source ~/.bashrc
