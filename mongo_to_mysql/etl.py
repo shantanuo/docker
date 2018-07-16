d8bd03ba6b8cc6f43d526d1732a9870973ea455b
Shantanu Oak <shantanu.oak@gmail.com>
2018-07-16 10:36:12 +0530

    added try block

--- mongo_to_mysql/etl.py
+++ mongo_to_mysql/etl.py
@@ -14,11 +14,12 @@ mystring='updated_'
 def convertdate(filename):
     df=pd.read_csv(filename) 
     df['message']=df.message.replace('\n', '', regex=True)
-
-    for i in ['sent_date', 'event_datetime', 'done_date', 'insert_date' ]:
-        df[i]=df[i].apply(lambda x: datetime.fromtimestamp(float(x)/1000))
-        pd.to_timedelta(df[i], errors = 'coerce')
-
+    try:
+        for i in ['sent_date', 'event_datetime', 'done_date', 'insert_date' ]:
+            df[i]=df[i].apply(lambda x: datetime.fromtimestamp(float(x)/1000))
+            pd.to_timedelta(df[i], errors = 'coerce')
+    except:
+        pass
     df.to_csv(mystring+filename, sep='\t', encoding='utf-8')
     s3_client.upload_file(mystring+filename, '13cols', 'csv1/'+mystring+filename)
 
