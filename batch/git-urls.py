b197e23b6cb918f856b9f52a39b96a44107b077a
Shantanu Oak <shantanu.oak@gmail.com>
2019-08-17 16:20:28 +0530

    Create git-urls.py

---
+++ batch/git-urls.py
@@ -0,0 +1,15 @@
+import feedparser
+import re
+from bs4 import BeautifulSoup
+import requests 
+
+api_url = "https://wgykjmyt0e.execute-api.us-east-1.amazonaws.com/stage"
+
+url = "https://towardsdatascience.com/feed"
+feed = feedparser.parse(url)
+
+for post in feed.entries:
+    soup = BeautifulSoup(post.description)
+    for link in soup.findAll("a", attrs={"href": re.compile("^https://github.com")}):
+        PARAMS = {'title':link.get("href")} 
+        r = requests.get(url = api_url, params = PARAMS)
