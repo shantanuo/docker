27818e066f22e539f2df4087065916f252b8b7a1
Shantanu Oak <shantanu.oak@gmail.com>
2021-07-18 10:07:21 +0000

    Create update_feed.sh

---
+++ wiki/update_feed.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget 'https://mr.wikipedia.org/w/api.php?hidebots=1&hidecategorization=1&hideWikibase=1&urlversion=2&days=14&limit=50&action=feedrecentchanges&feedformat=atom' -O /tmp/out1.txt
+ 
+PERL5OPT=-CA xpath -e "/feed/entry[author/name!='Aditya tamhankar' and author/name!='Dharmadhyaksha' and author/name!='नरेश सावे' and author/name!='अभय नातू']" /tmp/out1.txt > /tmp/updated.txt
