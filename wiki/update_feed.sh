1bcb473eb7f4d79054bb39f78e5d1d19f9e5e352
Shantanu Oak <shantanu.oak@gmail.com>
2021-09-19 15:24:34 +0530

    Update update_feed.sh

--- wiki/update_feed.sh
+++ wiki/update_feed.sh
@@ -2,7 +2,7 @@
 wget 'https://mr.wikipedia.org/w/api.php?hidebots=1&hidecategorization=1&hideWikibase=1&urlversion=2&days=30&limit=500&action=feedrecentchanges&feedformat=atom' -O /tmp/out1.txt
 
 head -9 /tmp/out1.txt > /tmp/updated.txt
-PERL5OPT=-CA xpath -e "/feed/entry[author/name!='Aditya tamhankar' and author/name!='Dharmadhyaksha' and author/name!='नरेश सावे' and author/name!='अभय नातू']" /tmp/out1.txt >> /tmp/updated.txt
+PERL5OPT=-CA xpath -e "/feed/entry[author/name!='Aditya tamhankar' and author/name!='Dharmadhyaksha' and author/name!='नरेश सावे' and author/name!='अभय नातू' and author/name!='संतोष गोरे']" /tmp/out1.txt >> /tmp/updated.txt
 tail -1 /tmp/out1.txt >> /tmp/updated.txt
 
 aws s3 cp /tmp/updated.txt s3://hunspell/wiki/ --acl public-read
